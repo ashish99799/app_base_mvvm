4e121ae2b13139522a2f0b9c6dc1714b421c7c5d
Ashish.V <ashish.v@deltaww.com>
2022-08-30 14:13:15 +0530

    Update some code.

--- app_base/build/generated/source/buildConfig/debug/com/application/base/BuildConfig.java
+++ app_base/build/generated/source/buildConfig/debug/com/application/base/BuildConfig.java
@@ -5,6 +5,8 @@ package com.application.base;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "com.application.base";
+  public static final String APPLICATION_ID = "com.application.base";
   public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.2";
 }
