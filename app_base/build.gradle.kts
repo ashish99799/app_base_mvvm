2ce63be1d0f243eddeacbd63a3f278bb3b49f63a
Ashish Vaishnav <ashish99799@gmail.com>
2023-11-22 16:16:45 +0530

    cline up the code.

--- app_base/build.gradle.kts
+++ app_base/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("kotlin-android")
-    id("maven-publish")
 }
 
 android {
@@ -41,17 +40,6 @@ android {
         abortOnError = false
         checkReleaseBuilds = false
     }
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))        // << --- ADD This
-    }
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17            // << --- ADD This
-    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
